cbf1fe52d04daaafc87124af1146d5c5b45b8acd
unknown <iamfarmanmalik@gmail.com>
2023-03-04 21:11:40 +0530

    built template driven form

---
+++ AngularForms/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularForms';
+  formClick(value:any){
+    console.log("Clicked",value)
+  }
+}
